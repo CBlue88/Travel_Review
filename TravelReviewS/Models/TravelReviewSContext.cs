f2936697b433d0c538cc011b60e2c64b2d7e14dc
Chanel Williams <chnlwllms8@gmail.com>
2017-02-13 09:30:31 -0500

    Final commit

---
+++ TravelReviewS/Models/TravelReviewSContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewS.Models
+{
+    public class TravelReviewSContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TravelReviewSContext() : base("name=TravelReviewSContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TravelReview.Models.Review> Reviews { get; set; }
+
+        public System.Data.Entity.DbSet<TravelReview.Models.Category> Categories { get; set; }
+    }
+}
